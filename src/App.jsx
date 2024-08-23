a18faf05f11ff113bbf9d8e26b954f3df129bf90
Yash rana <97224327+YashRana03@users.noreply.github.com>
2024-08-23 11:21:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="canvas"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
